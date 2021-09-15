0aa0eaea06e980b12cb084f4556751e7f813ed56
Ganesh Saini <sainiganesh.dev@gmail.com>
2021-09-15 12:56:14 +0530

    my upstream

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 hello i from master/main branch
 ok  so we  already push content to the remote branch
-so now we are in the dev branch  ok  
+so now we are in the dev branch  ok 
+after upstream branch 
